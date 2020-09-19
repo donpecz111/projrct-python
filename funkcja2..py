07e3aca25407c52d2319ff9badd28dfad0494271
donpecz111 <70844255+donpecz111@users.noreply.github.com>
2020-09-19 11:58:47 +0200

    Add files via upload

---
+++ funkcja2..py
@@ -0,0 +1,17 @@
+def powiedz(wiadomosc,ile = 1):
+    print(wiadomosc * ile)
+
+powiedz('Hej')
+powiedz('Hej' ,6)
+
+s = "Ala ma kota i 12 psów"
+print(len (s))
+print(s)
+s = s.replace('kota','krowę')
+print(s)
+s = s.lower()
+print(s.count('a'))
+s = s.upper()
+print(s)
+print(s)
+print(s.find('m'))
